80b84dcc627b85c6d3b33933015f10b3427e3b8a
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-26 23:49:12 +0300

    add dto

---
+++ src/main/java/ru/golikov/notes/dto/NoteDto.java
@@ -0,0 +1,26 @@
+package ru.golikov.notes.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class NoteDto {
+
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    @JsonProperty("create_at")
+    private LocalDate createAt;
+
+    @JsonProperty("update_at")
+    private LocalDate updateAt;
+
+    private Long userId;
+}
---
+++ src/main/java/ru/golikov/notes/dto/UserDto.java
@@ -0,0 +1,27 @@
+package ru.golikov.notes.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    private Long id;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("middle_name")
+    private String middleName;
+
+    private String email;
+
+    private String password;
+}
