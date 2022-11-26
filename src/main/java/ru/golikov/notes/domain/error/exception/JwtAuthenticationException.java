4d8d628dc4ef46712ec3b3d2eb64f384674c80ce
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-27 02:21:59 +0300

    refactor

--- src/main/java/ru/golikov/notes/domain/error/exception/JwtAuthenticationException.java
+++ src/main/java/ru/golikov/notes/domain/error/exception/JwtAuthenticationException.java
@@ -1,12 +1,8 @@
-package ru.golikov.notes.domain.security.jwt;
+package ru.golikov.notes.domain.error.exception;
 
 import org.springframework.security.core.AuthenticationException;
 
 public class JwtAuthenticationException extends AuthenticationException {
-
-    public JwtAuthenticationException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
 
     public JwtAuthenticationException(String msg) {
         super(msg);
