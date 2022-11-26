028bb501ffa93b6e8c8524a79ec235a0d8d3e3b8
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-27 02:24:15 +0300

    rename & refactor

---
+++ src/main/java/ru/golikov/notes/domain/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package ru.golikov.notes.domain.security.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import ru.golikov.notes.domain.security.model.UserDetailsImpl;
+import ru.golikov.notes.domain.user.entity.User;
+import ru.golikov.notes.domain.user.service.UserService;
+
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public UserDetailsServiceImpl(@Lazy UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userService.findByEmail(email);
+        return UserDetailsImpl.builder()
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .middleName(user.getMiddleName())
+                .password(user.getPassword())
+                .createAt(user.getCreateAt())
+                .updateAt(user.getUpdateAt())
+                .isActive(user.isActive())
+                .authorities(user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getRole()))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
