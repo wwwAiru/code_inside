59df378d7b8c9dee119a19a26c781ec2a16f3070
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-27 14:09:44 +0300

    add AdminController getAllUsers()

---
+++ src/main/java/ru/golikov/notes/domain/security/controller/AdminController.java
@@ -0,0 +1,26 @@
+package ru.golikov.notes.domain.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.golikov.notes.domain.user.dto.UserDto;
+import ru.golikov.notes.domain.user.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/all")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> allUsers = userService.findAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+}
