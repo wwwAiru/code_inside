27cc75ba9dc6e72fdc129f2ca8adf23fc2889c30
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-27 16:11:19 +0300

    refactor & add editUser()

--- src/main/java/ru/golikov/notes/domain/user/service/UserService.java
+++ src/main/java/ru/golikov/notes/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import ru.golikov.notes.domain.error.exception.NotFoundException;
 import ru.golikov.notes.domain.error.exception.UserRegistrationException;
 import ru.golikov.notes.domain.role.entity.Role;
@@ -11,10 +12,12 @@ import ru.golikov.notes.domain.role.repository.RoleRepository;
 import ru.golikov.notes.domain.user.dto.UserDto;
 import ru.golikov.notes.domain.user.entity.User;
 import ru.golikov.notes.domain.user.repository.UserRepository;
+import ru.golikov.notes.domain.util.UserMapper;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -51,36 +54,37 @@ public class UserService {
         }
     }
 
-    public List<UserDto> findAllUsers() {
-        return userRepository.findAll().stream()
-                .map(user -> UserDto.builder()
-                        .id(user.getId())
-                        .email(user.getEmail())
-                        .firstName(user.getFirstName())
-                        .lastName(user.getLastName())
-                        .middleName(user.getMiddleName())
-                        .createAt(user.getCreateAt())
-                        .updateAt(user.getUpdateAt())
-                        .roles(user.getRoles().stream().map(Role::getRole).collect(Collectors.toList()))
-                        .build())
+    public UserDto editUser(UserDto userDto) {
+        List<Role> roles = userDto.getRoles().stream()
+                .map(roleRepository::findByRole)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .collect(Collectors.toList());
+        User user = findById(userDto.getId());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setActive(userDto.isActive());
+        user.setEmail(userDto.getEmail());
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setMiddleName(userDto.getMiddleName());
+        user.setUpdateAt(LocalDateTime.now());
+        if(!CollectionUtils.isEmpty(roles)) {
+            user.setRoles(roles);
+        }
+        User savedUser = userRepository.save(user);
+        return UserMapper.toDto(savedUser);
     }
 
-    public UserDto findById(Long id) {
+    public List<UserDto> findAllUsers() {
+        return UserMapper.toDtoList(userRepository.findAll());
+    }
+
+    public User findById(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new NotFoundException(String.format("user with id = %d not found", id)));
         if (user == null) {
             log.warn(String.format("user with id = %d not found", id));
         }
-        return UserDto.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .middleName(user.getMiddleName())
-                .createAt(user.getCreateAt())
-                .updateAt(user.getUpdateAt())
-                .roles(user.getRoles().stream().map(Role::getRole).collect(Collectors.toList()))
-                .build();
+        return user;
     }
 
     public User findByEmail(String email) {
