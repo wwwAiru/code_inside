263b8b1a7bbff25b35d48101d541e7c71a2f37c6
wwwAiru <z-a-t-o@yandex.ru>
2022-11-28 13:26:38 +0300

    add toListDto(List<Note> notes)

--- src/main/java/ru/golikov/notes/util/NoteMapper.java
+++ src/main/java/ru/golikov/notes/util/NoteMapper.java
@@ -3,6 +3,9 @@ package ru.golikov.notes.util;
 import ru.golikov.notes.domain.note.dto.NoteDto;
 import ru.golikov.notes.domain.note.entity.Note;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class NoteMapper {
     public static NoteDto toDto(Note note) {
         return NoteDto.builder()
@@ -13,5 +16,11 @@ public class NoteMapper {
                 .updateAt(note.getUpdateAt())
                 .email(note.getUser().getEmail())
                 .build();
+    }
+
+    public static List<NoteDto> toListDto(List<Note> notes) {
+        return notes.stream()
+                .map(NoteMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
