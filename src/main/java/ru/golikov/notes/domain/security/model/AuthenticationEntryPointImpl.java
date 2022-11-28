d8b847041ac661e9e13f0eaf7a47ea1abd9f9912
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-29 02:04:09 +0300

    add AuthenticationEntryPointImpl for 403 response if token expired

---
+++ src/main/java/ru/golikov/notes/domain/security/model/AuthenticationEntryPointImpl.java
@@ -0,0 +1,45 @@
+package ru.golikov.notes.domain.security.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import ru.golikov.notes.domain.error.dto.ErrorDetailsDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(SC_FORBIDDEN);
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        String message;
+        if (e.getCause() != null) {
+            message = e.getCause().getMessage();
+        } else {
+            message = e.getMessage();
+        }
+        byte[] body = objectMapper.writeValueAsBytes(ErrorDetailsDto.builder()
+                .timestamp(LocalDateTime.now())
+                .message("The token may have expired")
+                .details(message)
+                .build()
+        );
+        httpServletResponse.getOutputStream().write(body);
+    }
+}
