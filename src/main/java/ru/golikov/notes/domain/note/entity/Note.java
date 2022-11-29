b6499874560f30bec79e1abd0507b1fd0a4c83c3
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-30 00:22:40 +0300

    add Audit

--- src/main/java/ru/golikov/notes/domain/note/entity/Note.java
+++ src/main/java/ru/golikov/notes/domain/note/entity/Note.java
@@ -3,16 +3,26 @@ package ru.golikov.notes.domain.note.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import ru.golikov.notes.domain.user.entity.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+
 
 @Entity
 @Table(name = "notes")
 @NoArgsConstructor
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
+@Audited
 public class Note {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -25,13 +35,25 @@ public class Note {
     @Column(name = "body")
     private String body;
 
+    @CreatedDate
     @Column(name = "create_at")
     private LocalDateTime createAt;
 
+    @LastModifiedDate
     @Column(name = "update_at")
     private LocalDateTime updateAt;
 
     @ManyToOne()
     @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @NotAudited
     private User user;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
+
 }
