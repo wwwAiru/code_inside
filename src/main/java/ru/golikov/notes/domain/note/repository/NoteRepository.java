fdf587b58509b703ab9af2ff656be51a6bc236b8
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-29 22:05:34 +0300

    optimize sql queries by EntityGraph

--- src/main/java/ru/golikov/notes/domain/note/repository/NoteRepository.java
+++ src/main/java/ru/golikov/notes/domain/note/repository/NoteRepository.java
@@ -2,6 +2,7 @@ package ru.golikov.notes.domain.note.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.golikov.notes.domain.note.entity.Note;
 import ru.golikov.notes.domain.user.entity.User;
@@ -9,6 +10,7 @@ import ru.golikov.notes.domain.user.entity.User;
 import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    @EntityGraph(attributePaths = {"user"})
     Page<Note> findAllByUser(User user, Pageable pageable);
 
     Optional<Note> findByIdAndUser(Long id, User user);
--- src/main/java/ru/golikov/notes/domain/user/repository/UserRepository.java
+++ src/main/java/ru/golikov/notes/domain/user/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package ru.golikov.notes.domain.user.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.golikov.notes.domain.user.entity.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     User findByEmail(String email);
+
+    @Override
+    @EntityGraph(attributePaths = {"roles"})
+    List<User> findAll();
 }
