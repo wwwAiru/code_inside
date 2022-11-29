a362e872b480679fbf1e368573a51109334c18bc
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-29 17:45:32 +0300

    add findByIdAndUser()

--- src/main/java/ru/golikov/notes/domain/note/repository/NoteRepository.java
+++ src/main/java/ru/golikov/notes/domain/note/repository/NoteRepository.java
@@ -5,7 +5,10 @@ import ru.golikov.notes.domain.note.entity.Note;
 import ru.golikov.notes.domain.user.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
     List<Note> findAllByUser(User user);
+
+    Optional<Note> findByIdAndUser(Long id, User user);
 }
