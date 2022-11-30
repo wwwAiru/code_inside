29e28cd5aedd25c3b0557d4f9b6e895e36f3d857
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-30 19:47:34 +0300

    refactor CustomRevisionEntity

--- src/main/java/ru/golikov/notes/domain/note/entity/CustomRevisionEntity.java
+++ src/main/java/ru/golikov/notes/domain/note/entity/CustomRevisionEntity.java
@@ -1,34 +1,39 @@
 package ru.golikov.notes.domain.note.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
 import org.hibernate.envers.RevisionNumber;
 import org.hibernate.envers.RevisionTimestamp;
+import ru.golikov.notes.audit.config.CustomListener;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
 @RevisionEntity(CustomListener.class)
 @Getter
 @Setter
-public class CustomRevisionEntity{
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"})
+public class CustomRevisionEntity {
 
     @Id
     @GeneratedValue
     @RevisionNumber
+    @JsonProperty("revision_id")
     private int id;
 
     @RevisionTimestamp
     private Date timestamp;
 
+    @JsonProperty("created_by")
     private String createdBy;
 
+    @JsonProperty("modified_by")
     private String modifiedBy;
 
 }
