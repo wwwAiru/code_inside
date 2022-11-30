973e0c10ea67bf1a4e997d06d27b4261c8eed99a
Andrey Golikov <z-a-t-o@yandex.ru>
2022-11-30 19:48:47 +0300

    add getNotesAudit()

--- src/main/java/ru/golikov/notes/controller/NoteController.java
+++ src/main/java/ru/golikov/notes/controller/NoteController.java
@@ -1,16 +1,13 @@
 package ru.golikov.notes.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.query.AuditEntity;
-import org.hibernate.envers.query.AuditQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import ru.golikov.notes.audit.service.AuditService;
 import ru.golikov.notes.domain.note.dto.NoteDto;
-import ru.golikov.notes.domain.note.entity.Note;
 import ru.golikov.notes.domain.note.service.NoteService;
 import ru.golikov.notes.domain.security.model.UserDetailsImpl;
 
@@ -23,7 +20,7 @@ public class NoteController {
 
     private final NoteService noteService;
 
-    private final AuditReader auditReader;
+    private final AuditService auditService;
 
     @PostMapping("/create")
     public ResponseEntity<NoteDto> createNote(@RequestBody NoteDto noteDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -50,15 +47,11 @@ public class NoteController {
         return new ResponseEntity<>(String.format("Note with id = %d deleted", id), HttpStatus.OK);
     }
 
-    @GetMapping("/audit")
-    public ResponseEntity<List> getNotesAudit(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        AuditQuery auditQuery = auditReader.createQuery().forRevisionsOfEntity(Note.class, false, true)
-                .add(AuditEntity.property("user_id").eq(userDetails.getId()))
-                .addProjection(AuditEntity.revisionType())
-                .addProjection(AuditEntity.id())
-                .addProjection(AuditEntity.selectEntity(false));
-        List resultList = auditQuery.getResultList();
-        resultList.forEach(System.out::println);
-        return new ResponseEntity<>(resultList, HttpStatus.OK);
+    @GetMapping("/audit/{id}")
+    public ResponseEntity<List<?>> getNotesAudit(@PathVariable Long id,
+                                                 @RequestParam boolean fetchChanges,
+                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<?> notesRevById = auditService.getNotesRevById(id, fetchChanges, userDetails);
+        return new ResponseEntity<>(notesRevById, HttpStatus.OK);
     }
 }
