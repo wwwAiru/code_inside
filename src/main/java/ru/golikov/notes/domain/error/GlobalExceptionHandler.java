6c7a9004b0e0694fc8318629b710cb2fc70ba833
Andrey Golikov <z-a-t-o@yandex.ru>
2022-12-01 21:54:18 +0300

    add validation handler

--- src/main/java/ru/golikov/notes/domain/error/GlobalExceptionHandler.java
+++ src/main/java/ru/golikov/notes/domain/error/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package ru.golikov.notes.domain.error;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -12,9 +15,38 @@ import ru.golikov.notes.domain.error.exception.NotFoundException;
 import ru.golikov.notes.domain.error.exception.UserRegistrationException;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	/**
+	 * Метод отлавливает ошибки валидации
+	 * @param ex - исключение вызванное валидацией
+	 * @param headers - заголовки
+	 * @param status - код
+	 * @param request - веб реквест
+	 * @return
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+																  HttpHeaders headers,
+																  HttpStatus status,
+																  WebRequest request) {
+		Map<String, Object> body = new LinkedHashMap<>();
+		Map<String, Set<String>> errors = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.collect(Collectors.groupingBy(FieldError::getField, Collectors.mapping(FieldError::getDefaultMessage, toSet())));
+		body.put("timestamp", LocalDateTime.now());
+		body.put("errors", errors);
+		return new ResponseEntity<>(body, headers, status);
+	}
 
 	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<?> notFoundException(NotFoundException ex, WebRequest request) {
