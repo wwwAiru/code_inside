9f2219ec0af9f71b21f5be2358776acdd76a274e
wwwAiru <z-a-t-o@yandex.ru>
2022-12-02 15:28:21 +0300

    add AbstractSpringBootTest

--- src/test/java/ru/golikov/notes/AbstractSpringBootTest.java
+++ src/test/java/ru/golikov/notes/AbstractSpringBootTest.java
@@ -1,13 +1,7 @@
 package ru.golikov.notes;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+public abstract class AbstractSpringBootTest {
 }
