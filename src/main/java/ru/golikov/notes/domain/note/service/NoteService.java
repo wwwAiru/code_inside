c971a36c96f4b3b58e5c327e9104c7ef3617fb8e
Andrey Golikov <z-a-t-o@yandex.ru>
2022-12-03 17:01:41 +0300

    fix

--- src/main/java/ru/golikov/notes/domain/note/service/NoteService.java
+++ src/main/java/ru/golikov/notes/domain/note/service/NoteService.java
@@ -1,7 +1,6 @@
 package ru.golikov.notes.domain.note.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
