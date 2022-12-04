7f3804174c628d7c782f2343dfe6c47051a21c16
Andrey Golikov <z-a-t-o@yandex.ru>
2022-12-05 02:42:34 +0300

    add ActiveProfile("test")

--- src/test/java/ru/golikov/notes/AbstractSpringBootTest.java
+++ src/test/java/ru/golikov/notes/AbstractSpringBootTest.java
@@ -1,7 +1,9 @@
 package ru.golikov.notes;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
 public abstract class AbstractSpringBootTest {
 }
