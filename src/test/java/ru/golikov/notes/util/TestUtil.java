03cf6e9c4bcd9e9b53bea463d94d6b63376f19ec
Andrey Golikov <z-a-t-o@yandex.ru>
2022-12-04 19:49:04 +0300

    add: createUserDto(); createRoleUser()

--- src/test/java/ru/golikov/notes/util/TestUtil.java
+++ src/test/java/ru/golikov/notes/util/TestUtil.java
@@ -2,7 +2,9 @@ package ru.golikov.notes.util;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import ru.golikov.notes.domain.note.entity.Note;
+import ru.golikov.notes.domain.role.entity.Role;
 import ru.golikov.notes.domain.security.model.UserDetailsImpl;
+import ru.golikov.notes.domain.user.dto.UserDto;
 import ru.golikov.notes.domain.user.entity.User;
 
 import java.time.LocalDateTime;
@@ -13,7 +15,7 @@ import java.util.stream.IntStream;
 
 public class TestUtil {
 
-    public static UserDetailsImpl createUserDetails(){
+    public static UserDetailsImpl createUserDetails() {
         LocalDateTime dateTime = LocalDateTime.now();
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
@@ -28,6 +30,24 @@ public class TestUtil {
                 .password(null)
                 .isActive(true)
                 .authorities(authorities)
+                .build();
+    }
+
+    public static UserDto createUserDto() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        List<String> roles = new ArrayList<>();
+        roles.add("ROLE_USER");
+        return UserDto.builder()
+                .id(1L)
+                .createAt(dateTime)
+                .updateAt(dateTime)
+                .firstName("Test")
+                .lastName("User")
+                .middleName("Userovich")
+                .email("test@user.ru")
+                .password(null)
+                .isActive(true)
+                .roles(roles)
                 .build();
     }
 
@@ -49,5 +69,11 @@ public class TestUtil {
         IntStream.range(0, 5).forEach(i -> noteList.add(createNoteByParams((long) i, "title"+i, "body"+i,
                 LocalDateTime.now(), LocalDateTime.now(), user)));
         return noteList;
+    }
+
+    public static Role createRoleUser() {
+        Role roleUser = new Role("ROLE_USER");
+        roleUser.setId(2L);
+        return roleUser;
     }
 }
